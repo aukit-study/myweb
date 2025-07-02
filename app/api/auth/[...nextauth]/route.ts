9a32a89af720b67585c530c93935a6c03ddfb4a2
aukit-study <aukit.tanti@gmail.com>
2025-07-02 15:07:15 +0700

    Update something

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,6 @@
-// app/api/auth/[...nextauth]/route.ts
+
 import NextAuth from 'next-auth'
-import type { NextAuthOptions } from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import bcrypt from 'bcryptjs'
-import db from '@/lib/db'
-import { RowDataPacket } from 'mysql2'
-
-// ตัวเลือกการตั้งค่าของ NextAuth
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
-        if (!credentials) return null
-
-        const { email, password } = credentials
-        const [rows] = await db.query('SELECT * FROM users WHERE email = ?', [email]) as [RowDataPacket[]]
-        const user = rows[0]
-
-        if (!user) return null
-
-        const isValid = await bcrypt.compare(password, user.password)
-        if (!isValid) return null
-
-        return {
-          id: user.id,
-          name: user.name || user.email,
-          email: user.email,
-        }
-      },
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: '/login',
-  },
-}
+import { authOptions } from '@/lib/auth'
 
 const handler = NextAuth(authOptions)
 
---
+++ lib/auth.ts
@@ -0,0 +1,42 @@
+// lib/auth.ts
+import type { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import bcrypt from 'bcryptjs'
+import db from '@/lib/db'
+import { RowDataPacket } from 'mysql2'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null
+
+        const { email, password } = credentials
+        const [rows] = await db.query('SELECT * FROM users WHERE email = ?', [email]) as [RowDataPacket[]]
+        const user = rows[0]
+        if (!user) return null
+
+        const isValid = await bcrypt.compare(password, user.password)
+        if (!isValid) return null
+
+        return {
+          id: user.id,
+          name: user.name || user.email,
+          email: user.email,
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+}
